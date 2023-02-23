2f2c893f7eb3bb5385502eb208c96b738706888a
Sungjun (June) Cho <scho361@gatech.edu>
2023-02-23 13:34:05 +0900

    Update run_baselines.sh

--- code/run_baselines.sh
+++ code/run_baselines.sh
@@ -2,6 +2,6 @@ for model in softmax nystrom-256 linformer-256 reformer-2 performer-256 linear
 do
     for task in listops text retrieval image pathfinder32-curv_contour_length_14
     do
-        python3 run_tasks.py --task $task --model $model --suffix run_baselines_$model
+        python3 run_tasks.py --task $task --model $model
     done
 done
